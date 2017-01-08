650a649e759ca04ebf3bba9cbcb9f1966a36998e
Prasanth Ramineni <prasanth270@gmail.com>
2017-01-08 13:48:31 -0800

    Party Rock UI

---
+++ PartyRock/PartyRockApp/PartyRockApp/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PartyRockApp
+//
+//  Created by Prasanth Ramineni on 1/7/17.
+//  Copyright © 2017 Prasanth Ramineni. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
+
---
+++ PartyRock/PartyRockApp/PartyRockApp/VideoViewController.swift
@@ -0,0 +1,29 @@
+//
+//  VideoViewController.swift
+//  PartyRockApp
+//
+//  Created by Prasanth Ramineni on 1/7/17.
+//  Copyright © 2017 Prasanth Ramineni. All rights reserved.
+//
+
+import UIKit
+
+class VideoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
