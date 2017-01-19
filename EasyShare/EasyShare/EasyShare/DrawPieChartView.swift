7e0bd0edfd5a4e118234580e6a283dd0d50a0996
Prasanth Ramineni <prasanth270@gmail.com>
2017-01-19 00:54:00 -0800

    Adding UI Design

--- EasyShare/EasyShare/EasyShare/DrawPieChartView.swift
+++ EasyShare/EasyShare/EasyShare/DrawPieChartView.swift
@@ -47,16 +47,6 @@ class DrawPieChartView: UIView {
         
         shapeLayer.fillColor = mainColor.cgColor
         
-//        let linePath = UIBezierPath()
-//        linePath.lineWidth = 1.0
-//        linePath.move(to: point)
-//        linePath.addLine(to: CGPoint(x: frame.width/2 + radius, y: frame.height/2))
-//        linePath.addArc(withCenter: point, radius: frame.width/2, startAngle: 0, endAngle: 3.14/2, clockwise: true)
-//        linePath.move(to: point)
-//        linePath.addLine(to: CGPoint(x: frame.width/2, y: frame.height/2 + radius))
-//        
-//        linePath.fill()
-        
         drawArc(centerPoint: point, startPoint: point1, endPoint: point2, radius: radius, startAngle: 0, endAngle: 3.14/2, lineWidth: 1, selectColor: 1)
         drawArc(centerPoint: point, startPoint: point2, endPoint: point3, radius: radius, startAngle: 3.14/2, endAngle: 3.14, lineWidth: 1, selectColor: 2)
         //layer.addSublayer(shapeLayer)
@@ -102,76 +92,5 @@ class DrawPieChartView: UIView {
         shapeLayer.lineWidth = ringThickness
         layer.addSublayer(shapeLayer)
     }
-    
-//    @IBInspectable var progress : Double =  0.0 {
-//        didSet {
-//            self.setNeedsDisplay()
-//        }
-//    }
-    
-//    required init?(coder aDecoder: NSCoder) {
-//        super.init(coder: aDecoder)
-//        self.contentMode = .redraw
-//    }
-//    
-//    override init(frame: CGRect) {
-//        super.init(frame: frame)
-//        self.backgroundColor =  UIColor.black
-//        self.contentMode = .redraw
-//    }
-    
-//    override func draw(_ rect: CGRect) {
-//        let color = UIColor.black
-//        let linewidth: CGFloat = 2.0
-//        
-//        let margin: CGFloat = linewidth
-//        let box0 = self.bounds.insetBy(dx: margin, dy: margin)
-//        let side: CGFloat = min(box0.width, box0.height)
-//        let box = CGRect(x: (self.bounds.width - side)/2, y: (self.bounds.height - side)/2, width: side, height: side)
-//        //let box = CGRectMake((self.bounds.width - side)/2, (self.bounds.height - side)/2, side, side)
-//        
-//        let ctx = UIGraphicsGetCurrentContext()
-//        
-//        ctx!.beginPath()
-//        //ctx!.setStrokeColor(color as! CGColor)
-//        ctx!.setLineWidth(linewidth)
-//        ctx!.addEllipse(in: box)
-//        ctx!.closePath()
-//        ctx!.strokePath()
-//        
-//        
-//        let delta: CGFloat = -CGFloat(M_PI_2)
-//        let radius: CGFloat = min(box.width, box.height)/2.0
-//        
-//        func prog_to_rad(p: Double) -> CGFloat {
-//            let rad = CGFloat(p * 2 * M_PI)
-//            return rad + delta
-//        }
-//
-//        func draw_arc(s: CGFloat, e: CGFloat, color: CGColor) {
-//            print("Helloooooo")
-//            ctx!.beginPath()
-//            let point = CGPoint(x: box.midX, y: box.midY)
-//            ctx!.move(to: point)
-//            //CGContextMoveToPoint(ctx, box.midX, box.midY)
-//            ctx!.setFillColor(color)
-//            
-//            ctx!.addArc(center: point, radius: radius - linewidth/2, startAngle: s, endAngle: 3, clockwise: true)
-//            
-////            CGContextAddArc(
-////                ctx,
-////                box.midX,
-////                box.midY,
-////                radius-linewidth/2,
-////                s,
-////                e,
-////                0)
-//            
-//            ctx!.closePath()
-//            ctx?.fillPath()
-//        }
-//        
-//        
-//    }
-    
+        
 }
