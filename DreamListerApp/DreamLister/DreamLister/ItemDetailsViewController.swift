55f053c51224549d8080d963ec7994c2c4d83a1e
Prasanth Ramineni <prasanth270@gmail.com>
2017-01-22 12:46:57 -0800

    Adding Segues and Add/Edit View

---
+++ DreamListerApp/DreamLister/DreamLister/ItemDetailsViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ItemDetailsViewController.swift
+//  DreamLister
+//
+//  Created by Prasanth Ramineni on 1/21/17.
+//  Copyright © 2017 Prasanth Ramineni. All rights reserved.
+//
+
+import UIKit
+
+class ItemDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        if let topItem = self.navigationController?.navigationBar.topItem {
+            topItem.backBarButtonItem = UIBarButtonItem(title: "", style: UIBarButtonItemStyle.plain, target: nil, action: nil)
+            
+        }
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
