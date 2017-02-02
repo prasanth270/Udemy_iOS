ecdb34e05a136420143249a4c420172ce670599a
Prasanth Ramineni <prasanth270@gmail.com>
2017-02-02 10:10:11 -0800

    Initial Commit fot the Weather App

---
+++ WeatherUpdate App/WeatherUpdates/WeatherUpdates/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherUpdates
+//
+//  Created by Prasanth Ramineni on 2/2/17.
+//  Copyright © 2017 Prasanth Ramineni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
